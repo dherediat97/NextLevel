7e0a2dcfa19cf99369ea141c2e045b5594f794c8
David <dherediat97@gmail.com>
2019-10-07 13:42:27 +0200

    cambios para que se cambie el color de la pestaña a la vez que se cambia el menu, server mas optimizado y  mas rapido

--- src/appPage/app.component.ts
+++ src/appPage/app.component.ts
@@ -1,25 +1,29 @@
 import { Component } from '@angular/core';
+import { MatTabChangeEvent } from '@angular/material';
+import { Router,ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'next-level-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor() {
+  constructor(private route:ActivatedRoute,private router:Router) {
   }
-  tabs = { backgroundcolor: "primary" };
+  tabs = { backgroundcolor: "accent" };
   tituloApp = "Next Level";
-  onSelectChange(event) {
-    console.log(event)
+  onSelectChange(event: MatTabChangeEvent) {
+    console.log('event => ', event);
+    console.log('index => ', event.index);
+    console.log('tab => ', event.tab);
     if (event.index == 0) {
       this.tabs.backgroundcolor = "primary";
-
+      this.router.navigate(['homePageLol']);
     } else if(event.index == 1) {
       this.tabs.backgroundcolor = "accent";
-
+      this.router.navigate(['homePagePaladins']);
     }else if(event.index == 2){
       this.tabs.backgroundcolor = "warn";
-
+      this.router.navigate(['homePageSmite']);
     }
   }
 }
