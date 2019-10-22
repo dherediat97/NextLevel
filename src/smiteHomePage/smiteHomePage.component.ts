c368183ad3e62c285541345e6e3882205b2b925b
David <dherediat97@gmail.com>
2019-10-22 16:57:57 +0200

    mejoras lista de dioes smite y detalles del dios

--- src/smiteHomePage/smiteHomePage.component.ts
+++ src/smiteHomePage/smiteHomePage.component.ts
@@ -3,20 +3,21 @@ import { HttpModule, Http } from '@angular/http';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
 import 'rxjs/add/operator/map'
+import { Router } from '@angular/router';
 @Component({
   selector: 'pagina-principal-smite',
   templateUrl: './smiteHomePage.component.html',
   styleUrls: ['./smiteHomePage.component.css']
 })
 export class HomePageSmiteComponent implements OnInit{
-  constructor(private http: Http) {
+  constructor(private http: Http, private router: Router) {
   }
   PROD:any = environment.production;
   dioses: any;
   tituloApp = "Next Level";
 
   detallesCampeon(id) {
-
+    this.router.navigate(['champion/smite/'+ id]);
   }
   ngOnInit(){
     this.obtenerDioses();
