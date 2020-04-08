37e0bdb72e9ee83309888725f89fa14d508bf35b
David Heredia <david.heredia@sensingassets.com>
2020-04-08 22:27:52 +0200

    Mejoras de estilo, eliminacion de codigo/funciones que no se usan

--- src/appPage/app.component.ts
+++ src/appPage/app.component.ts
@@ -1,32 +1,32 @@
 import { Component } from '@angular/core';
-import { Router,ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 @Component({
   selector: 'next-level-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private route:ActivatedRoute,private router:Router) {
+  constructor(private router: Router) {
   }
-  tabs = { backgroundcolor: "warn" , accentcolor:"warn"};
-  urlPortfolio:any = "https://dherediat97.github.io/SlimPortoflio/";
+  tabs = { backgroundcolor: "warn", accentcolor: "warn" };
+  urlPortfolio: any = "https://dherediat97.github.io/SlimPortoflio/";
   tituloApp = "Next Level";
-  onSelectChange(event) {
+  onSelectChange(event: any) {
     if (event.index == 0) {
       this.tabs.backgroundcolor = "warn";
       this.tabs.accentcolor = "warn";
       this.router.navigate(['homePageLol']);
-    } else if(event.index == 1) {
+    } else if (event.index == 1) {
       this.tabs.backgroundcolor = "accent";
       this.tabs.accentcolor = "accent";
       this.router.navigate(['homePagePaladins']);
-    }else if(event.index == 2){
+    } else if (event.index == 2) {
       this.tabs.backgroundcolor = "primary";
       this.tabs.accentcolor = "primary";
       this.router.navigate(['homePageSmite']);
     }
   }
-  openPortfolio(){
+  openPortfolio() {
     window.open(this.urlPortfolio, "_blank");
   }
 }
--- src/detailsChampionPage/detailsChampion.component.ts
+++ src/detailsChampionPage/detailsChampion.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Http } from '@angular/http';
-import { Router, ActivatedRoute } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { environment } from '../environments/environment';
 @Component({
   selector: 'detalle-pagina-campeon',
@@ -10,117 +10,117 @@ import { environment } from '../environments/environment';
 export class DetailsChampionComponent {
 
   //Riot Games
-  nombreCampeon:any;
+  nombreCampeon: any;
 
   //HiRez Games
   idCampeon: any;
-  PROD:any = environment.production;
+  PROD: any = environment.production;
   campeon: any;
   skinsCampeon: any;
   versionLol: any;
   juego: any;
 
-  constructor(private http: Http, private router: Router,private route: ActivatedRoute) {
+  constructor(private http: Http, private route: ActivatedRoute) {
     this.idCampeon = this.route.snapshot.params['id'];
     this.juego = this.route.snapshot.params['juego'];
     this.versionLol = window.localStorage.getItem('versionLol');
   }
 
-  ngOnInit(){
+  ngOnInit() {
     this.juego = this.route.snapshot.params['juego'];
-    if(this.juego=='paladins'){
+    if (this.juego == 'paladins') {
       this.idCampeon = this.route.snapshot.params['id'];
       this.cargarDatosCampeonPaladins();
-    }else if(this.juego=='smite'){
+    } else if (this.juego == 'smite') {
       this.nombreCampeon = this.route.snapshot.params['id'];
       this.cargarDatosCampeonSmite();
-    }else if(this.juego=='lol'){
+    } else if (this.juego == 'lol') {
       this.nombreCampeon = this.route.snapshot.params['id'];
       this.cargarDatosCampeonLol();
     }
   }
-  cargarDatosCampeonLol(){
-   this.nombreCampeon = this.route.snapshot.params['id'];
-   this.obtenerCampeonLol(this.nombreCampeon);
+  cargarDatosCampeonLol() {
+    this.nombreCampeon = this.route.snapshot.params['id'];
+    this.obtenerCampeonLol(this.nombreCampeon);
   }
-  cargarDatosCampeonSmite(){
-   this.idCampeon = this.route.snapshot.params['id'];
-   this.obtenerCampeonSmite(this.idCampeon);
-   this.obtenerSkinDiosSmite(this.idCampeon);
+  cargarDatosCampeonSmite() {
+    this.idCampeon = this.route.snapshot.params['id'];
+    this.obtenerCampeonSmite(this.idCampeon);
+    this.obtenerSkinDiosSmite(this.idCampeon);
   }
-  cargarDatosCampeonPaladins(){
-   this.idCampeon = this.route.snapshot.params['id'];
-   this.obtenerCampeonPaladins(this.idCampeon);
-   this.obtenerSkinCampeonPaladins(this.idCampeon);
+  cargarDatosCampeonPaladins() {
+    this.idCampeon = this.route.snapshot.params['id'];
+    this.obtenerCampeonPaladins(this.idCampeon);
+    this.obtenerSkinCampeonPaladins(this.idCampeon);
   }
-  obtenerSkinDiosSmite(idCampeon){
-    var url ="";
-    if(this.PROD){
+  obtenerSkinDiosSmite(idCampeon: any) {
+    var url = "";
+    if (this.PROD) {
       url = 'https://nextlevelserver.herokuapp.com/';
-    }else{
+    } else {
       url = 'http://localhost:8081/';
     }
-    this.http.get(url+'smite/obtenerSkinDios/'+idCampeon)
+    this.http.get(url + 'smite/obtenerSkinDios/' + idCampeon)
       .subscribe(
         res => this.skinsCampeon = res.json().skin
-    );
+      );
   }
-  obtenerSkinCampeonPaladins(idCampeon){
-    var url ="";
-    if(this.PROD){
+  obtenerSkinCampeonPaladins(idCampeon: any) {
+    var url = "";
+    if (this.PROD) {
       url = 'https://nextlevelserver.herokuapp.com/';
-    }else{
+    } else {
       url = 'http://localhost:8081/';
     }
-    this.http.get(url+'paladins/obtenerSkinCampeon/'+idCampeon)
+    this.http.get(url + 'paladins/obtenerSkinCampeon/' + idCampeon)
       .subscribe(
         res => this.skinsCampeon = res.json().skin
-    );
+      );
   }
-  obtenerCampeonSmite(idCampeon){
-    if(idCampeon != undefined){
-      var url ="";
-        if(this.PROD){
-          url = 'https://nextlevelserver.herokuapp.com/';
-        }else{
-          url = 'http://localhost:8081/';
-        }
-         //Cojer Datos Basicos del campeon (img y nombre)
-        this.http.get(url+'smite/obtenerDios/'+idCampeon)
-          .subscribe(
-            res => this.campeon = res.json().dios[0]
+  obtenerCampeonSmite(idCampeon: any) {
+    if (idCampeon != undefined) {
+      var url = "";
+      if (this.PROD) {
+        url = 'https://nextlevelserver.herokuapp.com/';
+      } else {
+        url = 'http://localhost:8081/';
+      }
+      //Cojer Datos Basicos del campeon (img y nombre)
+      this.http.get(url + 'smite/obtenerDios/' + idCampeon)
+        .subscribe(
+          res => this.campeon = res.json().dios[0]
         );
 
     }
   }
-  obtenerCampeonPaladins(idCampeon){
-    if(idCampeon != undefined){
-      var url ="";
-        if(this.PROD){
-          url = 'https://nextlevelserver.herokuapp.com/';
-        }else{
-          url = 'http://localhost:8081/';
-        }
-         //Cojer Datos Basicos del campeon (img y nombre)
-        this.http.get(url+'paladins/obtenerCampeon/'+idCampeon)
-          .subscribe(
-            res => this.campeon = res.json().campeon[0]
+  obtenerCampeonPaladins(idCampeon: any) {
+    if (idCampeon != undefined) {
+      var url = "";
+      if (this.PROD) {
+        url = 'https://nextlevelserver.herokuapp.com/';
+      } else {
+        url = 'http://localhost:8081/';
+      }
+      //Cojer Datos Basicos del campeon (img y nombre)
+      this.http.get(url + 'paladins/obtenerCampeon/' + idCampeon)
+        .subscribe(
+          res => this.campeon = res.json().campeon[0]
         );
 
     }
   }
-  obtenerCampeonLol(nombreCampeon){
-    if(nombreCampeon != undefined){
-      var url ="";
-        if(this.PROD){
-          url = 'https://nextlevelserver.herokuapp.com/';
-        }else{
-          url = 'http://localhost:8081/';
-        }
-         //Cojer Datos Basicos del campeon (img y nombre)
-        this.http.get(url+'lol/obtenerCampeon/'+nombreCampeon)
-          .subscribe(
-            res => this.campeon = res.json()
+  obtenerCampeonLol(nombreCampeon: any) {
+    if (nombreCampeon != undefined) {
+      var url = "";
+      if (this.PROD) {
+        url = 'https://nextlevelserver.herokuapp.com/';
+      } else {
+        url = 'http://localhost:8081/';
+      }
+      //Cojer Datos Basicos del campeon (img y nombre)
+      this.http.get(url + 'lol/obtenerCampeon/' + nombreCampeon)
+        .subscribe(
+          res => this.campeon = res.json()
         );
     }
   }
